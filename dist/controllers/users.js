2221b6ebf55991998cc82e8ffd751823815b058d
Pepo995 <pepoborras@gmail.com>
2023-03-07 11:29:31 -0300

    Creating and setting app and tools

---
+++ dist/controllers/users.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateUser = exports.deleteUser = exports.createUser = exports.getUserById = exports.getUsers = void 0;
+const user_1 = __importDefault(require("../models/user"));
+const getUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield user_1.default.findAll();
+    return users;
+});
+exports.getUsers = getUsers;
+const getUserById = (userId) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_1.default.findOne({
+        where: { id: userId },
+    });
+    return user;
+});
+exports.getUserById = getUserById;
+const createUser = (createUserParams) => __awaiter(void 0, void 0, void 0, function* () {
+    yield user_1.default.create(createUserParams);
+});
+exports.createUser = createUser;
+const deleteUser = (userId) => __awaiter(void 0, void 0, void 0, function* () {
+    yield user_1.default.destroy({
+        where: { id: userId },
+    });
+});
+exports.deleteUser = deleteUser;
+const updateUser = (userId, userUpdate) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_1.default.findOne({
+        where: { id: userId },
+    });
+    user === null || user === void 0 ? void 0 : user.update(userUpdate);
+    return user;
+});
+exports.updateUser = updateUser;
---
+++ src/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface CreateUserParams {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  username: string;
+  age: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
